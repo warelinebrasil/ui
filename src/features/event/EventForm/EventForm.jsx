339d5ae406d829f2685cc3077181f0d84a8240db
Rogério Huss <rogeriohuss@wareline.com.br>
2019-11-20 09:47:14 -0300

    fim da seção 4

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -12,7 +12,7 @@ class EventForm extends Component {
 
   handleFormSubmit = evt => {
     evt.preventDefault();
-    console.log(this.state);
+    this.props.createEvent(this.state);
   };
 
   handleInputChange = ({ target: { name, value } }) => {
